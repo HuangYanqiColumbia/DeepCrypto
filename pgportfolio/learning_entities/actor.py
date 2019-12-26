corrected bugs in the calculation of transaction costs; swiched activations to leaky_relu

pgportfolio/learning_entities/actor.py
@@ -15,6 +15,7 @@ formatter = logging.Formatter('%(name)s - %(levelname)s\n%(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 
 class ActorNetwork(object):
@@ -32,10 +33,11 @@ class ActorNetwork(object):
 
         self.close = tf.placeholder(tf.float32,[self._input_num, self._window_size+1, self.a_dim])
         close = self.close
-        pnl = self.out * ((close[:,1:,:] - close[:,:-1,:]) / close[:,:-1,:])[:,-1,:]
+        returns = ((close[:,1:,:] - close[:,:-1,:]) / close[:,:-1,:])
+        pnl = self.out * returns[:,-1,:]
         
         weight = self.inputs[2]
-        commission = tf.abs(self.out - weight) * self._config["training"]["trading_consumption"]
+        commission = tf.abs(self.out - weight * (returns[:, -2, :] + 1)) * self._config["training"]["trading_consumption"]
         
         returns = pnl - commission
         self.loss = - tf.reduce_mean(tf.reduce_sum(returns,axis=1),axis=0)
@@ -55,6 +57,21 @@ class ActorNetwork(object):
         self.out = self._net.output
 
     def train(self, inputs, close, a):
+        if logger.level <= logging.DEBUG:   
+            logger.debug(
+                self.sess.run(self._grads_and_vars[0][0], feed_dict={
+                    self.inputs[0]: inputs[0],
+                    self.inputs[1]: inputs[1],
+                    self.inputs[2]: a, 
+                    self.close: close
+            })[:, :, :, 2])
+            logger.debug(
+                self.sess.run(self._grads_and_vars[0][1], feed_dict={
+                    self.inputs[0]: inputs[0],
+                    self.inputs[1]: inputs[1],
+                    self.inputs[2]: a, 
+                    self.close: close
+            })[:, :, :, 2])
         self.sess.run(self.optimize, feed_dict={
             self.inputs[0]: inputs[0],
             self.inputs[1]: inputs[1],

pgportfolio/learning_entities/env.py
@@ -49,10 +49,9 @@ class Env():
         self._curr_ind += 1
         s2 = self._data[:,:,self._curr_ind-self._window_size+1:self._curr_ind+1]
         close = self._data[0,:,self._curr_ind-self._window_size:self._curr_ind+1].T
-        pnl = np.nansum(self.weight * (self._data[0,:,self._curr_ind]\
-                                       - self._data[0,:,self._curr_ind-1]) \
-                        / self._data[0,:,self._curr_ind-1])
-        commission = np.nansum(np.abs(self.weight - action) * self._config["training"]["trading_consumption"])
+        retrn = (self._data[0,:,self._curr_ind] - self._data[0,:,self._curr_ind-1]) / self._data[0,:,self._curr_ind-1]
+        pnl = np.nansum(self.weight * retrn)
+        commission = np.nansum(np.abs(self.weight * (retrn + 1) - action) * self._config["training"]["trading_consumption"])
         self.reward = pnl - commission
         self.weight = action
         if time_index:

pgportfolio/trainer/trainer.py
@@ -17,6 +17,7 @@ formatter = logging.Formatter('%(name)s - %(levelname)s\n%(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 class Trainer():
     def __init__(self, sess, config, global_step: int\
@@ -93,7 +94,7 @@ class Trainer():
         a = self._actor.predict([s[0][None,:],1,self._pvm[[-1,]]])
         if logger.level <= logging.DEBUG:
             logger.debug(
-                a
+                a[0, :5]
             )
         self._pvm = np.append(self._pvm, a, axis=0)
         s2, close = self._env.step(a[0])